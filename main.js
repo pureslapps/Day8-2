cc5a4ce7f06d43d31ecc9ee142ad1dd33ba6a8f7
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-01 16:57:44 -0400

    added markdown notes

--- main.js
+++ main.js
@@ -18,3 +18,38 @@ funtion ("name") {
   console.log(NAME)
   return name
 }
+
+
+## Operators in JavaScript
+
+is Equal to
+`==`
+This checks to see if the values on either side of the operand are equal to one another. Not to be confused with the assignment operator.
+
+not Equal to
+`!=`
+Checks to see if the values are not equivelent. Returns `true` if these values are different.
+
+less than
+`<`
+Checks to see if a value is less than another.
+
+Greater than or equal to
+`>=`
+Checks to see if a value is greater than or equal to another.
+
+Less than or equal to
+`<=`
+Checks to see if a value is less than or equal to another.
+
+AND
+`&&`
+Checks to see if both conditions on either side of the operator are true. Only if both of them are true, wiil this return "True"
+
+OR
+`||`
+Checks to see if either of the conditions are true. If at least 1 is true, this will return `true`
+
+TYPE AND VALUE check
+`===`
+Checks to see if a 2 values are equal in type and in value.

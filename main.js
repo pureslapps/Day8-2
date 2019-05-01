a458eb17f952a1040b858d5590c3a194fdc04aa7
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-01 16:45:32 -0400

    finished 2nd challenge

---
+++ main.js
@@ -0,0 +1,20 @@
+var person = promt("Please Enter your Name")
+var
+
+function greet(person) {
+  console.log(`${person} so nice to meet you!`)
+  return person
+}
+
+function network(int) {
+let name = greet(person)
+console.log(`${name} just networked with ${int}people`)
+}
+
+
+## Challenge 2
+
+funtion ("name") {
+  console.log(NAME)
+  return name
+}
